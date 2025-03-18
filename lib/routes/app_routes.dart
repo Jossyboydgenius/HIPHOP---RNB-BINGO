10172770b9ff6ec0c8a2a1c4876f2d95bf429ea9
jossyboydgenius <jossyboydgenius@gmail.com>
2025-03-18 19:25:20 +0000

    Update AppRoutes to include QR code scanner route and set it as the initial route, enhancing navigation and user experience.

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -2,17 +2,19 @@ import 'package:flutter/material.dart';
 import '../views/splash_screen.dart';
 import '../views/onboarding_screen.dart';
 import '../views/home_screen.dart';
-
+import '../views/qr_code_scanner_screen.dart';
 class AppRoutes {
   static const String splash = '/';
   static const String onboarding = '/onboarding';
   static const String home = '/home';
+  static const String qrCodeScanner = '/qrCodeScanner';
 
-  static const String initialRoute = splash;
+  static const String initialRoute = qrCodeScanner;
 
   static Map<String, WidgetBuilder> routes = {
     splash: (context) => const SplashScreen(),
     onboarding: (context) => const OnboardingScreen(),
     home: (context) => const HomeScreen(),
+    qrCodeScanner: (context) => const QRCodeScannerScreen(),
   };
 } 
