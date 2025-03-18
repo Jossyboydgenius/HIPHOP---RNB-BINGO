306ae96e7964841e517eb1e56dcfec70d8686e5c
jossyboydgenius <jossyboydgenius@gmail.com>
2025-03-18 08:13:26 +0000

    Add AppTextStyle and AppFontWeight classes to define text styles and font weights for consistent typography across the application

---
+++ lib/widgets/app_text_style.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+
+abstract class AppTextStyle {
+  // Font family names
+  static const String dmSansFont = 'DMSans';
+  static const String mochiyPopOneFont = 'MochiyPopOne';
+  static const String poppinsFont = 'Poppins';
+
+  // DMSans styles
+  static TextStyle get dmSansRegular => TextStyle(
+    fontFamily: dmSansFont,
+    fontWeight: AppFontWeight.regular,
+  );
+
+  static TextStyle get dmSansItalic => TextStyle(
+    fontFamily: dmSansFont,
+    fontStyle: FontStyle.italic,
+  );
+
+  // MochiyPopOne styles
+  static TextStyle get mochiyPopOneRegular => TextStyle(
+    fontFamily: mochiyPopOneFont,
+    fontWeight: AppFontWeight.regular,
+  );
+
+  // Poppins styles
+  static TextStyle get poppinsBlack => TextStyle(
+    fontFamily: poppinsFont,
+    fontWeight: AppFontWeight.black,
+  );
+
+  static TextStyle get poppinsBold => TextStyle(
+    fontFamily: poppinsFont,
+    fontWeight: AppFontWeight.bold,
+  );
+
+  static TextStyle get poppinsLight => TextStyle(
+    fontFamily: poppinsFont,
+    fontWeight: AppFontWeight.light,
+  );
+
+  static TextStyle get poppinsMedium => TextStyle(
+    fontFamily: poppinsFont,
+    fontWeight: AppFontWeight.medium,
+  );
+
+  static TextStyle get poppinsSemiBold => TextStyle(
+    fontFamily: poppinsFont,
+    fontWeight: AppFontWeight.semiBold,
+  );
+
+  static TextStyle get poppinsRegular => TextStyle(
+    fontFamily: poppinsFont,
+    fontWeight: AppFontWeight.regular,
+  );
+
+  // Helper methods for creating custom styles
+  static TextStyle dmSans({
+    double? fontSize,
+    FontWeight? fontWeight,
+    Color? color,
+    FontStyle? fontStyle,
+  }) => TextStyle(
+    fontFamily: dmSansFont,
+    fontSize: fontSize,
+    fontWeight: fontWeight,
+    color: color,
+    fontStyle: fontStyle,
+  );
+
+  static TextStyle mochiyPopOne({
+    double? fontSize,
+    FontWeight? fontWeight,
+    Color? color,
+  }) => TextStyle(
+    fontFamily: mochiyPopOneFont,
+    fontSize: fontSize,
+    fontWeight: fontWeight,
+    color: color,
+  );
+
+  static TextStyle poppins({
+    double? fontSize,
+    FontWeight? fontWeight,
+    Color? color,
+  }) => TextStyle(
+    fontFamily: poppinsFont,
+    fontSize: fontSize,
+    fontWeight: fontWeight,
+    color: color,
+  );
+}
+
+abstract class AppFontWeight {
+  /// FontWeight value of `w900`
+  static const FontWeight black = FontWeight.w900;
+
+  /// FontWeight value of `w800`
+  static const FontWeight extraBold = FontWeight.w800;
+
+  /// FontWeight value of `w700`
+  static const FontWeight bold = FontWeight.w700;
+
+  /// FontWeight value of `w600`
+  static const FontWeight semiBold = FontWeight.w600;
+
+  /// FontWeight value of `w500`
+  static const FontWeight medium = FontWeight.w500;
+
+  /// FontWeight value of `w400`
+  static const FontWeight regular = FontWeight.w400;
+
+  /// FontWeight value of `w300`
+  static const FontWeight light = FontWeight.w300;
+
+  /// FontWeight value of `w200`
+  static const FontWeight extraLight = FontWeight.w200;
+
+  /// FontWeight value of `w100`
+  static const FontWeight thin = FontWeight.w100;
+} 
