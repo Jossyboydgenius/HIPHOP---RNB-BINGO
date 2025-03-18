736236d3df2e64d0136b07bfa8011a0a298ee310
jossyboydgenius <jossyboydgenius@gmail.com>
2025-03-18 18:40:08 +0000

    Add home route to AppRoutes for navigation to HomeScreen, enhancing app structure and user flow.

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
 import '../views/splash_screen.dart';
 import '../views/onboarding_screen.dart';
+import '../views/home_screen.dart';
 
 class AppRoutes {
   static const String splash = '/';
   static const String onboarding = '/onboarding';
+  static const String home = '/home';
 
   static const String initialRoute = splash;
 
   static Map<String, WidgetBuilder> routes = {
     splash: (context) => const SplashScreen(),
     onboarding: (context) => const OnboardingScreen(),
+    home: (context) => const HomeScreen(),
   };
 } 
