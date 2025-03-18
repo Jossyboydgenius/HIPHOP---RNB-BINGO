ccc973a302e03c989648acae269f4e18456621b8
jossyboydgenius <jossyboydgenius@gmail.com>
2025-03-18 08:13:11 +0000

    Add AppDimension class to manage responsive design by calculating screen dimensions and device type

---
+++ lib/widgets/app_sizer.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AppDimension {
+  static late MediaQueryData mediaQuery;
+  static late double height;
+  static late double width;
+  static late bool isSmall;
+  static late bool isTablet;
+
+  static void init(BuildContext context) {
+    mediaQuery = MediaQuery.of(context);
+    height = mediaQuery.size.height;
+    width = mediaQuery.size.width;
+    isSmall = height < 700 && width < 400;
+    isTablet = width > 600;
+  }
+} 
