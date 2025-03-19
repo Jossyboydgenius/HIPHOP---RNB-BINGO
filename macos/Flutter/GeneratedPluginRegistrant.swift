81790aa84419fc34edddfeffb9dbd3deacb8faa0
jossyboydgenius <jossyboydgenius@gmail.com>
2025-03-19 01:14:20 +0000

    Update dependencies and platform configurations to enhance functionality and compatibility. Added image_picker and file_selector plugins for improved media selection across platforms. Updated iOS deployment target to 15.5 and registered new plugins in generated files for Linux, macOS, and Windows.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,6 +6,7 @@ import FlutterMacOS
 import Foundation
 
 import audioplayers_darwin
+import file_selector_macos
 import mobile_scanner
 import path_provider_foundation
 import rive_common
@@ -14,6 +15,7 @@ import sqflite_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   RivePlugin.register(with: registry.registrar(forPlugin: "RivePlugin"))
