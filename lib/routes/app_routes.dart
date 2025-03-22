257fa9adebe289797a95c77e5578165ff5e094e2
jossyboydgenius <jossyboydgenius@gmail.com>
2025-03-22 03:18:04 +0000

    Update AppRoutes: Replace remoteCode route with inputCode, and enhance route handling for QR code scanner, game details, and remote game details screens to support argument passing, improving navigation and user experience.

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -4,7 +4,7 @@ import '../views/onboarding_screen.dart';
 import '../views/home_screen.dart';
 import '../views/qr_code_scanner_screen.dart';
 import '../views/game_details_screen.dart';
-import '../views/remote_code_screen.dart';
+import '../views/input_code_screen.dart';
 import '../views/remote_game_details_screen.dart';
 
 class AppRoutes {
@@ -13,7 +13,7 @@ class AppRoutes {
   static const String home = '/home';
   static const String qrCodeScanner = '/qrCodeScanner';
   static const String gameDetails = '/gameDetails';
-  static const String remoteCode = '/remoteCode';
+  static const String inputCode = '/inputCode';
   static const String remoteGameDetails = '/remoteGameDetails';
 
   static const String initialRoute = splash;
@@ -22,9 +22,18 @@ class AppRoutes {
     splash: (context) => const SplashScreen(),
     onboarding: (context) => const OnboardingScreen(),
     home: (context) => const HomeScreen(),
-    qrCodeScanner: (context) => const QRCodeScannerScreen(),
-    gameDetails: (context) => const GameDetailsScreen(),
-    remoteCode: (context) => const RemoteCodeScreen(),
-    remoteGameDetails: (context) => const RemoteGameDetailsScreen(),
+    qrCodeScanner: (context) => const QRCodeScannerScreen(isInPerson: false), // Default to remote
+    gameDetails: (context) {
+      final args = ModalRoute.of(context)?.settings.arguments as String?;
+      return GameDetailsScreen(code: args);
+    },
+    inputCode: (context) {
+      final args = ModalRoute.of(context)?.settings.arguments as bool?;
+      return InputCodeScreen(isInPerson: args ?? false);
+    },
+    remoteGameDetails: (context) {
+      final args = ModalRoute.of(context)?.settings.arguments as String?;
+      return RemoteGameDetailsScreen(code: args);
+    },
   };
 } 
