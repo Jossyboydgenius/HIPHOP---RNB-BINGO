956234defa307f26de3284c820f57831e57a9abd
jossyboydgenius <jossyboydgenius@gmail.com>
2025-03-25 16:20:30 +0000

    Integrate AppDimension for responsive design in PaymentOptionsModal: Adjust dimensions, padding, and font sizes for improved adaptability across different screen sizes.

--- lib/widgets/payment_options_modal.dart
+++ lib/widgets/payment_options_modal.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hiphop_rnb_bingo/widgets/app_icons.dart';
+import 'package:hiphop_rnb_bingo/widgets/app_sizer.dart';
 import 'dart:ui';
 import 'app_modal_container.dart';
 import 'app_colors.dart';
 import 'app_text_style.dart';
 import 'app_images.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class PaymentOptionsModal extends StatelessWidget {
   final VoidCallback onClose;
@@ -19,18 +20,31 @@ class PaymentOptionsModal extends StatelessWidget {
     this.isInAppPurchase = false,
   });
 
+  final Map<String, Map<String, dynamic>> paymentOptions = const {
+    'PayPal': {
+      'icon': AppImageData.paypal,
+    },
+    'CashApp': {
+      'icon': AppImageData.cashapp,
+    },
+    'Zelle': {
+      'icon': AppImageData.zelle,
+    },
+  };
+
   Widget _buildTitle() {
     return Text(
       isInAppPurchase ? 'Fund from' : 'Pay Fees',
       style: AppTextStyle.poppins(
-        fontSize: 20.sp,
-        fontWeight: FontWeight.bold,
+        fontSize: AppDimension.isSmall ? 24.sp : 20.sp,
+        fontWeight: FontWeight.w800,
         color: Colors.white,
       ),
     );
   }
 
-  Widget _buildPaymentOption(String icon, String title) {
+  Widget _buildPaymentOption(String title) {
+    final option = paymentOptions[title]!;
     return Material(
       color: Colors.transparent,
       child: InkWell(
@@ -39,28 +53,27 @@ class PaymentOptionsModal extends StatelessWidget {
           onClose();
         },
         child: Padding(
-          padding: EdgeInsets.symmetric(vertical: 10.h),
+          padding: EdgeInsets.symmetric(vertical: AppDimension.isSmall ? 10.h : 10.h),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Row(
                 children: [
                   Container(
-                    width: 48.w,
-                    height: 48.h,
+                    width: AppDimension.isSmall ? 58.w : 48.w,
+                    height: AppDimension.isSmall ? 58.h : 48.h,
                     decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(12.r),
+                      borderRadius: BorderRadius.circular(AppDimension.isSmall ? 16.r : 12.r),
                       image: DecorationImage(
-                        image: AssetImage(icon),
-                        fit: BoxFit.cover,
+                        image: AssetImage(option['icon']),
                       ),
                     ),
                   ),
-                  SizedBox(width: 16.w),
+                  SizedBox(width: AppDimension.isSmall ? 20.w : 16.w),
                   Text(
                     title,
                     style: AppTextStyle.dmSans(
-                      fontSize: 16.sp,
+                      fontSize: AppDimension.isSmall ? 20.sp : 16.sp,
                       fontWeight: FontWeight.w600,
                       color: Colors.black,
                     ),
@@ -70,7 +83,7 @@ class PaymentOptionsModal extends StatelessWidget {
               AppIcons(
                 icon: AppIconData.arrowRight,
                 color: Colors.black,
-                size: 24.w,
+                size: AppDimension.isSmall ? 28.w : 24.w,
               ),
             ],
           ),
@@ -87,41 +100,54 @@ class PaymentOptionsModal extends StatelessWidget {
         filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
         child: Padding(
           padding: EdgeInsets.symmetric(horizontal: 24.w),
-          child: Center(
-            child: AppModalContainer(
-              width: double.infinity,
-              height: 300.h,
-              fillColor: AppColors.purplePrimary,
-              borderColor: AppColors.purpleLight,
-              layerColor: AppColors.purpleDark,
-              layerTopPosition: -4.h,
-              borderRadius: 32.r,
-              title: '',
-              customTitle: _buildTitle(),
-              onClose: onClose,
-              child: Padding(
-                padding: EdgeInsets.all(16.r),
-                child: Container(
-                  width: 320.w,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(16.r),
-                  ),
-                  child: Padding(
-                    padding: EdgeInsets.symmetric(
-                        horizontal: 16.w, vertical: 16.h),
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        _buildPaymentOption(AppImageData.paypal, 'PayPal'),
-                        _buildPaymentOption(AppImageData.cashapp, 'CashApp'),
-                        _buildPaymentOption(AppImageData.zelle, 'Zelle'),
-                      ],
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              AppModalContainer(
+                width: double.infinity,
+                height: AppDimension.isSmall ? 580.h : 300.h,
+                fillColor: AppColors.purplePrimary,
+                borderColor: AppColors.purpleLight,
+                layerColor: AppColors.purpleDark,
+                layerTopPosition: -4.h,
+                borderRadius: AppDimension.isSmall ? 32.r : 24.r,
+                title: '',
+                customTitle: _buildTitle(),
+                onClose: onClose,
+                child: Column(
+                  children: [
+                    Expanded(
+                      child: Padding(
+                        padding: EdgeInsets.all(AppDimension.isSmall ? 24.r : 16.r),
+                        child: Column(
+                          children: [
+                            Expanded(
+                              child: Container(
+                                width: double.infinity,
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(AppDimension.isSmall ? 20.r : 16.r),
+                                ),
+                                child: SingleChildScrollView(
+                                  child: Padding(
+                                    padding: EdgeInsets.all(AppDimension.isSmall ? 24.r : 16.r),
+                                    child: Column(
+                                      children: paymentOptions.keys
+                                          .map((title) => _buildPaymentOption(title))
+                                          .toList(),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
                     ),
-                  ),
+                  ],
                 ),
               ),
-            ),
+            ],
           ),
         ),
       ),
