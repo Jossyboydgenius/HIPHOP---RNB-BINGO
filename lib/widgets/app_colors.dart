25a8acd14537d3696135c9eac7505336b9e2a0c6
jossyboydgenius <jossyboydgenius@gmail.com>
2025-03-26 16:30:37 +0000

    Add new color constants to AppColors for enhanced theming options

--- lib/widgets/app_colors.dart
+++ lib/widgets/app_colors.dart
@@ -14,6 +14,7 @@ class AppColors {
   static const Color yellowDark2 = Color(0xFFFFB213);
   static const Color yellowDark3 = Color(0xFFFF8413);
   static const Color yellowDark4 = Color(0xFFFFB828);
+  static const Color deepYellow = Color(0xFFE9AD29);
 
   // Purple shades
   static const Color purplePrimary = Color(0xFFB26BFB);
@@ -24,6 +25,7 @@ class AppColors {
   static const Color purpleOverlay = Color(0xFFD3D8FF);
   static const Color purpleTransparent = Color(0x26D3D8FF);
   static const Color darkPurple = Color(0xFF7031FC);
+  static const Color darkPurple1 = Color(0xFF8550F1);
   static const Color darkPurple2 = Color(0xFF9A4AFE);
   static const Color darkPurple3 = Color(0xFFA75CF4);
 
@@ -37,6 +39,7 @@ class AppColors {
   static const Color greenBright = Color(0xFF67EB00);
   static const Color greenLight = Color(0xFFF4FFE4);
   static const Color greenDark = Color(0xFF4EC307);
+  static const Color deepGreen = Color(0xFF52B342);
   static const Color teal = Color(0xFF3FD6B3);
 
   // Background colors
@@ -49,18 +52,20 @@ class AppColors {
   static const Color pinkDark = Color(0xFFE90038);
   static const Color pinkDark2 = Color(0xFFE530B6);
   static const Color pinkLight = Color(0xFFFF79EA);
+  static const Color deepPink = Color(0xFFD53BFF);
   static const Color pinkLight2 = Color(0xFFF989FF);
   static const Color redLight = Color(0xFFF9F0F2);
 
   // Blue shades
   static const Color bluePrimary = Color(0xFF39C7FF);
   static const Color blueLight = Color(0xFF4CDAFE);
-  static const Color blueDark = Color(0xFF08B9FF);
-  static const Color blueDark2 = Color(0xFF05A4E8);
   static const Color blueLight2 = Color(0xFF53C1FF);
-  static const Color deepBlue = Color(0xFF098AEE);
   static const Color blueLight3 = Color(0xFF67FCFF);
   static const Color blueLight4 = Color(0xFF95E9FF);
+  static const Color blueDark = Color(0xFF08B9FF);
+  static const Color blueDark2 = Color(0xFF05A4E8);
+  static const Color deepBlue = Color(0xFF098AEE);
+  static const Color deepBlue1 = Color(0xFF1E88E5);
 
   // Pink/Purple gradient colors
   static const Color gradientPink = Color(0xFFFDA5FF);
