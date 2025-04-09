a748d97ca2a8104dff891db99c5cc7298f6b3c34
jossyboydgenius <jossyboydgenius@gmail.com>
2025-04-09 07:24:10 +0000

    Add service locator setup for dependency management
    This commit introduces the locator.dart file, which sets up a service locator using the GetIt package. It registers various services including Api, AuthService, GoogleAuthService, AppleAuthService, FacebookAuthService, and LocalStorageService, along with the AppFlavorConfig and FlutterSecureStorage. This enhancement improves the application's architecture by centralizing dependency management and facilitating easier service access throughout the app.

---
+++ lib/app/locator.dart
@@ -0,0 +1,45 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:get_it/get_it.dart';
+import '../data/services/api/api.dart';
+import '../services/auth_service.dart';
+import '../services/google_auth_service.dart';
+import '../services/apple_auth_service.dart';
+import '../services/facebook_auth_service.dart';
+import '../data/services/local_storage_service.dart';
+import 'flavor_config.dart';
+
+final locator = GetIt.instance;
+
+Future<void> setupLocator(AppFlavorConfig config) async {
+  await _registerExternalDependencies(config);
+  _registerServices();
+}
+
+void _registerServices() {
+  if (!locator.isRegistered<Api>()) {
+    locator.registerLazySingleton<Api>(() => Api());
+  }
+  if (!locator.isRegistered<AuthService>()) {
+    locator.registerLazySingleton<AuthService>(() => AuthService());
+  }
+  if (!locator.isRegistered<GoogleAuthService>()) {
+    locator.registerLazySingleton<GoogleAuthService>(() => GoogleAuthService());
+  }
+  if (!locator.isRegistered<AppleAuthService>()) {
+    locator.registerLazySingleton<AppleAuthService>(() => AppleAuthService());
+  }
+  if (!locator.isRegistered<FacebookAuthService>()) {
+    locator.registerLazySingleton<FacebookAuthService>(
+        () => FacebookAuthService());
+  }
+  if (!locator.isRegistered<LocalStorageService>()) {
+    locator.registerLazySingleton<LocalStorageService>(
+        () => LocalStorageService());
+  }
+}
+
+Future<void> _registerExternalDependencies(AppFlavorConfig config) async {
+  locator.registerLazySingleton<AppFlavorConfig>(() => config);
+  locator.registerLazySingleton<FlutterSecureStorage>(
+      () => const FlutterSecureStorage());
+}
