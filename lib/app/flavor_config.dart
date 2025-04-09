64d8db88566dd0f2cbeaad302a9bc30e3b1e9618
jossyboydgenius <jossyboydgenius@gmail.com>
2025-04-09 07:24:01 +0000

    Add AppFlavorConfig class for managing application flavor settings
    This commit introduces the AppFlavorConfig class, which encapsulates configuration details such as the app name, API base URL, web URL, and Mixpanel token. This addition enhances the application's ability to manage different environments and flavors, improving overall configuration management.

---
+++ lib/app/flavor_config.dart
@@ -0,0 +1,13 @@
+class AppFlavorConfig {
+  final String name;
+  final String apiBaseUrl;
+  final String webUrl;
+  final String mixpanelToken;
+
+  AppFlavorConfig({
+    required this.name,
+    required this.apiBaseUrl,
+    required this.webUrl,
+    required this.mixpanelToken,
+  });
+}
